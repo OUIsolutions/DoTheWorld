350ddfae6b2f87ac97b33b130e0df966c5b0d7e3
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-03-05 00:58:21 -0300

    created base functions

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include "functions/list_dirs.c"
+#include "functions/list_files.c"
+#include "functions/list_all.c"
+#include "functions/memory.c"
 
 int main(int argc, char *argv[]){
     int size;
-    char **dirs = cfm_list_dirs("exemple", &size);
+    char **dirs = cfm_list_all("exemple/", &size);
     for (int i = 0; i < size; i++) {
         printf("%s\n", dirs[i]);
     }
-
+    cfm_free_string_array(dirs, size);
+    
 }
