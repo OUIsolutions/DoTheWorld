0f99296f90502b4798fdd8eb3cc23e3cb96bbdb0
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-03-05 05:54:18 -0300

    at

--- functions/list_dirs_recursively.c
+++ functions/list_dirs_recursively.c
@@ -1,3 +1,4 @@
+#pragma once
 #include "extras.c"
 #include "monodimension_listage.c"
 
--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include "functions/wrappers.c"
 #include "functions/list_dirs_recursively.c"
+#include "functions/list_files_recursively.c"
 #include "functions/extras.c"
 
 
 
 int main(int argc, char *argv[]){
     int size;
-    char **dirs = dtw_list_dirs_recursively("/home/jurandi/Documentos", &size);
+    char **files = dtw_list_files_recursively("exemple", &size);
     for(int i = 0; i < size; i++){
-        printf("%s\n", dirs[i]);
+        printf("%s\n", files[i]);
     }
 }
