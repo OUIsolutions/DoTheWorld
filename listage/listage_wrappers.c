c8fa43156bffe300823c7156e4b305e90099021d
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-03-08 07:11:12 -0300

    at

---
+++ listage/listage_wrappers.c
@@ -0,0 +1,13 @@
+#include "monodimension_listage.c" 
+
+struct DtwStringArray * dtw_list_files(char *path, bool concat_path){
+    return dtw_list_basic(path,  "file", concat_path);
+}
+
+struct DtwStringArray * dtw_list_dirs(char *path, bool concat_path){
+    return dtw_list_basic(path,"dir", concat_path);
+}
+
+struct DtwStringArray *  dtw_list_all(char *path,  bool concat_path){
+    return dtw_list_basic(path, "all", concat_path);
+}
