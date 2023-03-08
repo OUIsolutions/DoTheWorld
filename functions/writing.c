a02bd32507541c76608e5ae0a1323229c65b58cc
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-03-08 07:42:28 -0300

    at

--- functions/writing.c
+++ functions/writing.c
@@ -8,14 +8,21 @@
 
 void dtw_create_dir_recursively(char *path){
     bool check = mkdir(path, 0777);
-    char current_path[100];
+    char * current_path = malloc(0);
     for(int i=0;i < strlen(path);i++){
         
         if(path[i] == '/' && i != strlen(path) - 1){
+            current_path = realloc(current_path,i);
             strncpy(current_path,path,i);
             mkdir(current_path, 0777);
         }
     }
+    free(current_path);
     
     mkdir(path, 0777);
+}
+
+
+void create_file_recursively(char *path,char *content){
+    
 }
