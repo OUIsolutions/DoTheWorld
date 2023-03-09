5caf29040d5edb22b0d954f981097f9b94083fa8
Mateus Moutinho Queiroz <61179749+mateusmoutinho@users.noreply.github.com>
2023-03-09 09:47:59 -0300

    creating tree part

--- code/structs/tree.c
+++ code/structs/tree.c
@@ -1,5 +1,5 @@
 
-struct DtwTree{
+struct DtwTreePart{
     unsigned int size;
     char *path;
     char *sha256;
@@ -9,19 +9,19 @@ struct DtwTree{
 
 };
 
-void dtw_set_tree_content(struct DtwTree *self, char *content){
+void dtw_set_tree_part_content(struct DtwTreePart *self, char *content){
     self->content = (char*)realloc(self->content,strlen(content) + 1);
     strcpy(self->content,content);
 }
 
-void dtw_load_tree_content(struct DtwTree *self, char *path){
+void dtw_load_tree_part_content(struct DtwTreePart *self, char *path){
      char *content = dtw_load_file_content(path);
     dtw_set_tree_content(self,content);
     free(content);
 }
 
-struct  DtwTree * dtw_create_tree(char *path,bool load_content){
-    struct DtwTree *self = (struct DtwTree*)malloc(sizeof(struct DtwTree));
+struct  DtwTreePart * dtw_create_tree(char *path,bool load_content){
+    struct DtwTreePart *self = (struct DtwTreePart*)malloc(sizeof(struct DtwTreePart));
     self->size = 0;
     self->path = (char*)malloc(strlen(path) + 1);
     strcpy(self->path, path);
@@ -30,18 +30,18 @@ struct  DtwTree * dtw_create_tree(char *path,bool load_content){
     self->ignore_all = false;
     self->ignore_content = false;
     if(load_content){
-        dtw_load_tree_content(self,path);
+        dtw_load_tree_part_content(self,path);
     }   
     return self;
 }
 
-void dtw_represent_tree(struct DtwTree *self){
+void dtw_represent_tree_part(struct DtwTreePart *self){
     printf("Path: %s\n", self->path);
     printf("SHA256: %s\n", self->sha256);
     printf("Content: %s\n", self->content);
 }
 
-void dtw_free_tree(struct DtwTree *self){
+void dtw_free_tree_part(struct DtwTreePart *self){
     free(self->path);
     free(self->sha256);
     free(self->content);
