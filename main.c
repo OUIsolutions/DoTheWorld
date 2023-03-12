ba97a2a947c28e5748d66eccefd4702558e9c629
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-03-12 04:50:02 -0300

    missing path

--- main.c
+++ main.c
@@ -2,5 +2,8 @@
 #include "code/doTheWorldDevUsage.c"
 
 int main(int argc, char *argv[]){
-
+    struct DtwTreePart *tree_part = dtw_create_tree_part(
+        "README.md",true,true    );
+        
+    dtw_represent_tree_part(tree_part);
 }
