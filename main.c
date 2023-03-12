183289ab77338ecd70e5fad74a1a7f30cb0d7665
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-03-12 13:54:25 -0300

    creating makefile

--- main.c
+++ main.c
@@ -2,12 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include "code/doTheWorldDevUsage.c"
+#include "code/doTheWorldSeparated.h"
 
 int main(int argc, char *argv[]){
-    
-    const char *frase = "eai beleza , ta tudo certo? beleza";
-    char * nova = dtw_replace_string(frase, "beleza", "legal");
-    printf("%s", nova);
+
 
 }
