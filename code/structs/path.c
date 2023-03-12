ff167d4d16e2c79e644ae3220826bc8417209d8b
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-03-12 05:39:59 -0300

    creating dtw Path

--- code/structs/path.c
+++ code/structs/path.c
@@ -9,8 +9,13 @@ struct DtwPath {
 };
 
 
-
-void dtw_create_path(struct DtwPath *self, const char *path) {
+void dtw_create_path(struct DtwPath *self, const char *ful_path) {
     self->full_path = (char *)malloc(0);
+    self->path = (char *)malloc(0);
+    self->full_name = (char *)malloc(0);
+    self->name = (char *)malloc(0);
+    self->extension = (char *)malloc(0);
 }
+
+
 
