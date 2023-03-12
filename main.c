579fb9948ecae49d49d595ca32e5d57b6fb7123d
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-03-12 05:57:05 -0300

    creating object oriented mode

--- main.c
+++ main.c
@@ -1,10 +1,7 @@
-
-#include "code/doTheWorldDevUsage.c"
+#include "code/imports.h"
+#include "code/structs/string_array.c"
 
 int main(int argc, char *argv[]){
-    struct DtwTreePart *tree_part = dtw_create_tree_part("README.md",true,true);
+ 
     
-    dtw_represent_tree_part(tree_part);
-    dtw_free_tree_part(tree_part);
-    return 0;
 }
