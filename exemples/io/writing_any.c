759cca7bf37b64d68579987e2532c51ec888b718
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-03-13 20:35:22 -0300

    at

--- exemples/io/writing_any.c
+++ exemples/io/writing_any.c
@@ -4,9 +4,14 @@
 int main(int argc, char *argv[]){
 
   //load the beer image
+  const char *deer_path = "../exemple_folder/deer.jpg";
   int deer_size;
-  char *deer_content = dtw_load_binary_content("../exemple_folder/deer.jpg",&deer_size);
+  char *deer_content = dtw_load_binary_content(deer_path,&deer_size);
   //use these functions for binary files
+  if(deer_content == NULL){
+    printf("error oppening %s\n",deer_path);
+    return 1;
+  }
   printf("size: %d\n",deer_size);
 
   bool result = dtw_write_any_content("exemple/deer.jpg",deer_content,deer_size);
