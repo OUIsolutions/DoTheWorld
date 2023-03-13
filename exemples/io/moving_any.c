d012c44e33377b54dc83fffd6e3e44c012aa71c1
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-03-13 20:51:14 -0300

    at

---
+++ exemples/io/moving_any.c
@@ -0,0 +1,16 @@
+
+
+#include "../../doTheWorld.h"
+
+int main(int argc, char *argv[]){
+  //| if you set merge to true,
+  //| the content of the folder will be merged 
+  //| with the content of the destination folder
+  bool merge = false;
+  dtw_move_any("../exemple_folder","exemple_folder_copy",merge);
+  
+  //| you can use move_any to move a file
+  dtw_move_any("../exemple_folder_copy/deer.jpg","deer.jpg",false);
+
+  return 0;
+}
