573fb32b929ac89495fe2a055b769812c73032b3
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-03-13 18:46:19 -0300

    creating exemples

---
+++ exemples/io/writing_strings.c
@@ -0,0 +1,7 @@
+
+#include "../../doTheWorld.h"
+
+int main(int argc, char *argv[]){
+  // Write a string to a file the path is auto created
+  bool result = dtw_write_string_file_content("teste/test.txt","Hello World!");
+}
