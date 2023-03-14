65c6a49d0d7b55e1d3c66dbb1ed7563d1a9ff09f
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-03-14 05:09:32 -0300

    starting hardware write

--- main.c
+++ main.c
@@ -2,10 +2,8 @@
 #include "code/doTheWorldSeparated.h"
 
 int main(int argc, char *argv[]){
-
-    struct DtwTreePart *tree = dtw_tree_part_constructor("README.md",true);
-    tree->set_string_content(tree,"Salve");
-    //tree->free_content(tree);
+    dtw_write_string_file_content("teste.aa","") ;
+    struct DtwTreePart *tree = dtw_tree_part_constructor("teste.aa",false);
     tree->represent(tree);
     tree->delete(tree);
 }
