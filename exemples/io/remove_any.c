5c461ed202551f20b1996bce967199ba08738a7c
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-03-13 21:16:35 -0300

    at

--- exemples/io/remove_any.c
+++ exemples/io/remove_any.c
@@ -3,12 +3,12 @@
 #include "../../doTheWorld.h"
 
 int main(int argc, char *argv[]){
-  //| Delete any will delete anything you passs to it
+  //| remove any will delete anything you passs to it
 
   //| if you pass a file, it will delete it
-  dtw_delete_any("../exemple_folder/deer.jpg");
+  dtw_remove_any("../exemple_folder/deer.jpg");
 
   //| if you pass a folder, it will delete it
-  dtw_delete_any("../exemple_folder/a");
+  dtw_remove_any("../exemple_folder/a");
   return 0;
 }
