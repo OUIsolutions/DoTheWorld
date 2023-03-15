0ed03223ca45a803df60bbbc870de9c3bac13011
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-03-15 10:34:23 -0300

    created hardware modifications

---
+++ README.md
@@ -0,0 +1,1 @@
+Havia conteudo aqui
--- main.c
+++ main.c
@@ -6,13 +6,14 @@ int main(int argc, char *argv[]){
     struct DtwTreePart *tree_part = dtw_tree_part_constructor(
         "README.md",
         true,
-        true
+        true  
     );
-    tree_part->set_string_content(tree_part,"");
-    tree_part->path->set_name(tree_part->path, "teste");
-    tree_part->hardware_modify(tree_part);
+    //tree_part->set_string_content(tree_part,"");
+    tree_part->path->set_name(tree_part->path, "README");
+    bool modifyed = tree_part->hardware_modify(tree_part);
+    printf("modifyed: %d\n",modifyed);
+    //tree_part->represent(tree_part);
     tree_part->delete_tree_part(tree_part);
-
     
 
 
