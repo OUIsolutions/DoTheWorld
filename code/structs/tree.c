1e4b4fcaeaaccaac3779e0a98f5fc808543f419b
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-03-15 01:38:06 -0300

    at

--- code/structs/tree.c
+++ code/structs/tree.c
@@ -3,10 +3,13 @@ struct  DtwTree{
     int size;
     struct DtwTreePart **tree_parts;
     void (*add_tree_part_by_copy)(struct DtwTree *self, struct DtwTreePart *tree_part);
+    void (*add_tree_part_by_referene)(struct DtwTree *self, struct DtwTreePart *tree_part);
     void (*delete_tree)(struct DtwTree *self);
     void(*represent)(struct DtwTree *self);
 };
+
 void private_dtw_add_tree_part_copy(struct DtwTree *self, struct DtwTreePart *tree_part);
+void private_dtw_add_tree_part_reference(struct DtwTree *self, struct DtwTreePart *tree_part);
 void private_dtw_delete_tree(struct DtwTree *self);
 void private_dtw_represent_tree(struct DtwTree *self);
 
@@ -16,6 +19,7 @@ struct  DtwTree * dtw_tree_constructor(){
     self->size = 0;
     self->tree_parts = (struct DtwTreePart**)malloc(1);
     self->add_tree_part_by_copy = private_dtw_add_tree_part_copy;
+    self->add_tree_part_by_referene = private_dtw_add_tree_part_reference;
     self->delete_tree = private_dtw_delete_tree;
     self->represent = private_dtw_represent_tree;
     return self;
@@ -25,8 +29,12 @@ void private_dtw_add_tree_part_copy(struct DtwTree *self, struct DtwTreePart *tr
     self->size++;
     self->tree_parts =  (struct DtwTreePart**)realloc(self->tree_parts, self->size * sizeof(struct DtwTreePart *));
     self->tree_parts[self->size - 1] = tree_part->copy_tree_part(tree_part);
-    
-    
+       
+}
+void private_dtw_add_tree_part_reference(struct DtwTree *self, struct DtwTreePart *tree_part){
+    self->size++;
+    self->tree_parts =  (struct DtwTreePart**)realloc(self->tree_parts, self->size * sizeof(struct DtwTreePart *));
+    self->tree_parts[self->size - 1] = tree_part;
 }
 
 void private_dtw_delete_tree(struct DtwTree *self){
