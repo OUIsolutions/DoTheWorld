4cc33bd3367a33c3629605fd5ea3f6bd28f11433
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-03-17 05:52:13 -0300

    at

--- main.c
+++ main.c
@@ -15,15 +15,10 @@ void load_and_dump_string(){
 int main(int argc, char *argv[]){
     
 
-    struct DtwPath *path = dtw_constructor_path("/home/alex/Projects/teste.c");
-    char *full_path = path->get_path(path);
-    printf("%s",full_path);
-    free(full_path);
-    path->delete_path(path);
-
-
-    //char *content_json = tree->dumps_json_tree(tree,false,false,false,false,false);
-    //dtw_write_string_file_content("code.json",content_json);
-    //tree->delete_tree(tree);
-    //free(content_json);
+    struct DtwTree *tree = dtw_tree_constructor();
+    tree->add_path_from_hardware(tree,"",true,true);
+    char *content_json = tree->dumps_json_tree(tree,true,true,true,true,false);
+    dtw_write_string_file_content("code.json",content_json);
+    tree->delete_tree(tree);
+    free(content_json);
 }
