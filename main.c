c7ae480555a11762b3a654f96946dcad32ffe8fa
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-03-16 22:22:39 -0300

    starting creating loading tree json

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 
 int main(int argc, char *argv[]){
  
-    
+    struct DtwTree *tree = dtw_tree_constructor();
 
+    char *json_text = tree->dumps_tree_json(tree,true,true,false, false, false);
+    dtw_write_string_file_content("exemples.json", json_text);
+    free(json_text);
 }
