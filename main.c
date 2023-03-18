20824b517936e0aad8c10607ee388753ab6eab15
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-03-18 20:27:12 -0300

    att

--- main.c
+++ main.c
@@ -6,7 +6,8 @@ int main(int argc, char *argv[]){
     
     struct DtwTree *tree = dtw_tree_constructor();
     
-    tree->add_tree_from_hardware(tree,"exemples",DTW_LOAD_CONTENT,DTW_PRESERVE_CONTENT);
+    //tree->add_tree_from_hardware(tree,"exemples",DTW_LOAD_CONTENT,DTW_PRESERVE_CONTENT);
+    /*
     for(int i = 0; i < tree->size; i++){
         tree->tree_parts[i]->hardware_write(tree->tree_parts[i],DTW_SET_AS_ACTION);
         tree->tree_parts[i]->ignore = true;
@@ -18,16 +19,15 @@ int main(int argc, char *argv[]){
         DTW_PRESERVE_HARDWARE_DATA,
         DTW_PRESERVE_CONTENT_DATA,
         DTW_NOT_MINIFY,
-        DTW_NOT_CONSIDER_IGNORE
+        DTW_CONSIDER_IGNORE
     );
 
     dtw_write_string_file_content("exemples.json",json_string);
-    
-   /*
+    */
     char *json_string = dtw_load_string_file_content("exemples.json");
     tree->loads_json_tree(tree,json_string);
     tree->represent(tree);
-    */
+    
     tree->free_tree(tree);
     return 0;
 }
