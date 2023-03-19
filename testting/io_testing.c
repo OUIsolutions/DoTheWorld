74c0185b6000f8f07a8eca909ea782f17d675c98
Mateus Moutinho Queiroz <61179749+mateusmoutinho@users.noreply.github.com>
2023-03-19 19:56:46 -0300

    starting testage

---
+++ testting/io_testing.c
@@ -0,0 +1,5 @@
+
+
+void test_io_operations(){
+    dtw_create_dir("test");
+}
--- testting/main_test.c
+++ testting/main_test.c
@@ -1,8 +1,11 @@
 
 #include "../doTheWorld.c"
+#include "io_testing.c"
+
 
 
 int main(int argc, char *argv[]){
     
+    test_io_operations();
     return 0;
 }
