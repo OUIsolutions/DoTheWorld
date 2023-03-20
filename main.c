be6b9dffa78cd4c2555ad8317c7d854c137207bf
Mateus Moutinho Queiroz <61179749+mateusmoutinho@users.noreply.github.com>
2023-03-20 04:13:17 -0300

    fixing bugs

--- main.c
+++ main.c
@@ -1,9 +1,19 @@
-
-#include "doTheWorld/doTheWorldMain.c"
-
+#include "doTheWorld.c"
 
 int main(int argc, char *argv[]){
-    
-    
-    return 0;
+ 
+  const char *path = "exemple_folder/deer.jpg";
+  int size;
+  bool is_binary;
+  //load any file, is useful if you don't know if the file is binary or not
+  unsigned char *content = dtw_load_any_content(path,&size,&is_binary);
+  if(content == NULL){
+    printf("error oppening %s\n",path);
+    return 1;
+  }
+  printf("size: %d\n",size);
+  printf("is_binary: %s\n",is_binary ? "true" : "false");
+  printf("content: %s\n",content);
+  free(content);
+  return 0;
 }
