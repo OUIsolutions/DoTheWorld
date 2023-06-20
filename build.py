993b80a73c797c43bf054e94ec883480b75ee6ae
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-06-19 23:37:46 -0300

    att

--- build.py
+++ build.py
@@ -1,23 +1,18 @@
 
 import CToolKit as ct
 from os import remove
-from testing.test_io import  test_io_operations
 
 
 
-from testing.test_io import test_io_operations
+
 STARTER = 'doTheWorld/doTheWorldMain.h'
 OUTPUT_TEST = 'doTheWorld_test.h'
 
 ct.generate_amalgamated_code(STARTER,OUTPUT_TEST)
-try:
-    test_io_operations()
-    ct.generate_amalgamated_code(STARTER,'doTheWorld.h')
-except Exception as e:
-    print(e)
-    remove(OUTPUT_TEST)
 
-try:
-    remove(OUTPUT_TEST)
-except:pass
+ct.execute_test_for_folder('gcc','exemples/io/')
+
+
+
+ct.generate_amalgamated_code(STARTER,'doTheWorld.h')
 
