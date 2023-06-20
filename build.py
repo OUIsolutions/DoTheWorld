d2116e2225b5dae65e8e8c1b281674fc7e2cce0b
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-06-20 00:04:24 -0300

    at

--- build.py
+++ build.py
@@ -10,16 +10,13 @@ STARTER = 'doTheWorld/doTheWorldMain.h'
 OUTPUT_TEST = 'doTheWorld_test.h'
 
 ct.generate_amalgamated_code(STARTER,OUTPUT_TEST)
-copytree("exemple_folder","security_exemple")
+
 try:
     ct.execute_test_for_folder('gcc','exemples/io/')
     ct.generate_amalgamated_code(STARTER,'doTheWorld.h')
 except Exception as e:
     print(e)
 
-rmtree("exemple_folder")
-copytree("security_exemple","exemple_folder")
-rmtree("security_exemple")
 
 
 
