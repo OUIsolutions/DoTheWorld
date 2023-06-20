38a77831d2e5a580666816931e3f8cec22173b26
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-06-20 03:48:58 -0300

    att

--- build.py
+++ build.py
@@ -40,14 +40,13 @@ def execute_test_for_folder(folder:str):
 
 
 def  execute_all_tests():
-    execute_test_for_folder('exemples/path')
-    execute_test_for_folder('exemples/tree_parts')
-    execute_test_for_folder('exemples/trees')
     execute_test_for_folder('exemples/io')
     execute_test_for_folder('exemples/monodimension_listage')
     execute_test_for_folder('exemples/multidimension_listage')
     execute_test_for_folder('exemples/extras')
-
+    execute_test_for_folder('exemples/path')
+    execute_test_for_folder('exemples/tree_parts')
+    execute_test_for_folder('exemples/trees')
 
 
 
@@ -67,12 +66,14 @@ def generate_exemple_formated_for_readme():
     create_readme_files_of_folder('exemples/io')
     create_readme_files_of_folder('exemples/monodimension_listage')
     create_readme_files_of_folder('exemples/multidimension_listage')
-
+    create_readme_files_of_folder('exemples/path')
+    create_readme_files_of_folder('exemples/tree_parts')
+    create_readme_files_of_folder('exemples/trees')
 
 
 ct.generate_amalgamated_code(STARTER,OUTPUT_TEST)
 execute_all_tests()
-#generate_exemple_formated_for_readme()
+generate_exemple_formated_for_readme()
 #ct.include_code_in_markdown('README.md',True)
 ct.generate_amalgamated_code(STARTER,'doTheWorld.h')
 
