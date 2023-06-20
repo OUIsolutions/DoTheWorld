0e5ae97548e6da717a77a7c0edd67dee0526d6ee
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-06-20 00:05:09 -0300

    at

--- exemples/monodimension_listage/list_all.c
+++ exemples/monodimension_listage/list_all.c
@@ -1,11 +1,11 @@
 
 
 
-#include "doTheWorld.c"
+#include "../../doTheWorld_test.h"
 
 int main(int argc, char *argv[]){
 
-  struct DtwStringArray *all = dtw_list_all("exemple_folder",DTW_CONCAT_PATH);
+  DtwStringArray *all = dtw_list_all("exemple_folder",DTW_CONCAT_PATH);
   all->represent(all);
   return 0;
 }
