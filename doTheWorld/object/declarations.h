55f462ab306cb6e64d5576cc2a7669462f3158b8
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-06-27 09:09:46 -0300

    att

--- doTheWorld/object/declarations.h
+++ doTheWorld/object/declarations.h
@@ -1,3 +1,2 @@
 #include "sub_object/sub_object.h"
-#include "sub_object_array/sub_object_array.h"
 #include "object/object.h"
--- doTheWorld/object/definitons.h
+++ doTheWorld/object/definitons.h
@@ -1,3 +1,2 @@
 #include "sub_object/sub_object.c"
-#include "sub_object_array/sub_object_array.c"
 #include "object/object.c"
