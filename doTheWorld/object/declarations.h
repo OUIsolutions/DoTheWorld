71ddbea6dd6cd9858152ae97240ee1802a264599
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-06-27 17:00:49 -0300

    attt

--- doTheWorld/object/declarations.h
+++ doTheWorld/object/declarations.h
@@ -1,3 +1,5 @@
 //#include "sub_object/sub_object.h"
 //#include "sub_object_key_vall/sub_object_key_val.h"
+#include "garbage/garbage.h"
+#include "garbage_array/garbage_array.h"
 #include "object/object.h"
---
+++ doTheWorld/object/garbage_array/garbage_array.c
@@ -0,0 +1,5 @@
+//
+// Created by jurandi on 27-06-2023.
+//
+
+#include "garbage_array.h"
