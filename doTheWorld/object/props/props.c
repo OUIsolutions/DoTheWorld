ca74bc60286f68e5d7be131eb70bfab2612bdd4e
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-02 01:14:24 -0300

    att

--- doTheWorld/object/props/props.c
+++ doTheWorld/object/props/props.c
@@ -10,11 +10,26 @@ DtwObjectProps DtwObjectProps_create_props(DtwObjectProps *props){
     if(!result.garbage){
         result.garbage =DTW_ALLOW_GARBAGE;
     }
+    if(!result.cache){
+        result.cache = DTW_ALLOW_CACHE;
+    }
+
     return result;
 
 }
 
 
-DtwObjectProps no_store = {
+DtwObjectProps no_garbage = {
         .garbage = DTW_NOT_GARBAGE
 };
+
+
+DtwObjectProps no_cache= {
+        .cache =DTW_NOT_CACHE
+};
+
+DtwObjectProps no_store= {
+        .garbage = DTW_NOT_GARBAGE,
+        .cache =DTW_NOT_CACHE
+};
+
--- doTheWorld/object/props/props.h
+++ doTheWorld/object/props/props.h
@@ -1,10 +1,15 @@
 
-
+#define DTW_NOT_CACHE 1
+#define DTW_ALLOW_CACHE 2
 #define DTW_NOT_GARBAGE 1
 #define DTW_ALLOW_GARBAGE 2
 
 typedef struct DtwObjectProps{
+
+    int cache;
     int garbage;
+    int transaction;
+
 }DtwObjectProps;
 
 DtwObjectProps DtwObjectProps_create_props(DtwObjectProps *props);
