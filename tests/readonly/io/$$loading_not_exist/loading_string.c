afdf6ed4decb95c53af4afa4f2d88a35a5407444
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-06 17:50:03 -0300

    att

---
+++ tests/readonly/io/$$loading_not_exist/loading_string.c
@@ -0,0 +1,19 @@
+
+#include "../../doTheWorld_test.h"
+
+
+//#include "../../doTheWorld_test.h"
+
+int main(int argc, char *argv[]){
+  //load a string file content
+  const char *path = "tests/target/aaaaa";
+  char *content = dtw_load_string_file_content(path);
+  if(content == NULL){
+    printf("error oppening %s\n",path);
+    return 1;
+  }
+  printf("content: %s\n",content);
+  free(content);
+  return 0;
+  
+}
--- tests/readonly/io/$$loading_string/loading_string.c
+++ tests/readonly/io/$$loading_string/loading_string.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[]){
   //load a string file content
-  const char *path = "exemple_folder/a/test.txt";
+  const char *path = "tests/target/a.txt";
   char *content = dtw_load_string_file_content(path);
   if(content == NULL){
     printf("error oppening %s\n",path);
@@ -12,4 +12,5 @@ int main(int argc, char *argv[]){
   printf("content: %s\n",content);
   free(content);
   return 0;
+  
 }
