b9d5a8e6de10496edf97e302a01d7ddc2fac67fa
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-08 18:11:21 -0400

    at

--- tests/main_test/io/R_write_any/exec.c
+++ tests/main_test/io/R_write_any/exec.c
@@ -2,17 +2,17 @@
 
 int main(int argc, char *argv[]){
   //load the beer image
-  const char *deer_path = "test/target/blob.png";
-  long deer_size;
-  unsigned char *content = dtw_load_binary_content(deer_path,&deer_size);
+  const char *blob_path = "tests/target/blob.png";
+  long blob_size;
+  unsigned char *content = dtw_load_binary_content(blob_path,&blob_size);
   //use these functions for binary files
   if(content == NULL){
-    printf("error oppening %s\n",deer_path);
+    printf("error oppening %s\n",blob_path);
     return 1;
   }
-  printf("size: %ld\n",deer_size);
+  printf("size: %ld\n",blob_size);
 
-  bool result = dtw_write_any_content("output_folder/blob2.png",content,deer_size);
+  bool result = dtw_write_any_content("target/tests/blob2.png",content,blob_size);
   printf("result: %s\n",result ? "true" : "false");
   free(content);
   return 0;
