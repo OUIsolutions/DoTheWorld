e70a72cd64968444de7b36394dfb37056fcc3930
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-08 17:53:57 -0400

    at

--- tests/main_test/io/write_any/writing_any.c
+++ tests/main_test/io/write_any/writing_any.c
@@ -1,8 +1,8 @@
-#include "../../doTheWorld_test.h"
+#include "../../../doTheWorld_test.h"
 
 int main(int argc, char *argv[]){
   //load the beer image
-  const char *deer_path = "exemple_folder/deer.jpg";
+  const char *deer_path = "test/target/blob.png";
   long deer_size;
   unsigned char *content = dtw_load_binary_content(deer_path,&deer_size);
   //use these functions for binary files
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]){
   }
   printf("size: %ld\n",deer_size);
 
-  bool result = dtw_write_any_content("output_folder/deer.jpg",content,deer_size);
+  bool result = dtw_write_any_content("output_folder/blob2.png",content,deer_size);
   printf("result: %s\n",result ? "true" : "false");
   free(content);
   return 0;
