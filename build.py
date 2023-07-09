126f71b4b3681ba89c140c210cef0894c16fe9a5
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-09 16:47:14 -0400

    att

--- build.py
+++ build.py
@@ -16,7 +16,7 @@ ct.generate_amalgamated_code(STARTER,OUTPUT_TEST)
 
 test = ct.FolderTestPreset(folder='tests/main_test',side_effect_folder='tests/target')
 test.generate_ouptut()
-test.start_test()
+#test.start_test()
 
 
 
