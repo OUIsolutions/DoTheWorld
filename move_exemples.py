22d1cda985bcc87d1a3df52faced607b45b98987
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-09 22:22:11 -0400

    at

--- move_exemples.py
+++ move_exemples.py
@@ -6,5 +6,5 @@ content  = listdir('exemples')
 for c in content:
     path = f'exemples/{c}'
     for p in listdir(path):
-        novo_path = f'{path}/{p}'
-        print(novo_path)
+        formatado = f'{path.replace("exemples","tests/main_test")}/T_{p.replace(".c","")}'
+        print(formatado)
