e688a367622f2f3658755812cc45bbc1fe3dcb91
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-09 22:29:48 -0400

    at

--- move_exemples.py
+++ move_exemples.py
@@ -1,5 +1,5 @@
 from os import listdir ,makedirs
-from os.path import isdir
+from os.path import isdir,isfile
 #from shutil import makedirs
 
 content  = listdir('exemples')
@@ -10,9 +10,13 @@ for c in content:
         caminho_do_arquivo = f'{path}/{p}'
         nova_pasta = f'{path.replace("exemples","tests/main_test")}/T_{p.replace(".c","")}'
         novo_caminho = f'{nova_pasta}/exec.c'
-        if isdir(nova_pasta):
-            print('ja existe',nova_pasta)
-            continue 
-        #fazendo alteracoes
-        makedirs(nova_pasta)
+        if not  isdir(nova_pasta):
+            makedirs(nova_pasta)
+
+        if not isfile(novo_caminho):
+            with open(caminho_do_arquivo,'r' ) as arq:
+                conteudo = arq.read()
+                with open(novo_caminho,'w') as arq::
+                    arq.write(conteudo)
+        
         
