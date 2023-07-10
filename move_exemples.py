06ed36dfba8f0bf652cdd0c25601dd47474aa2df
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-09 22:26:03 -0400

    at

--- move_exemples.py
+++ move_exemples.py
@@ -1,11 +1,16 @@
 from os import listdir
-
+from os.path import isdir
+#from shutil import makedirs
 
 content  = listdir('exemples')
 
 for c in content:
     path = f'exemples/{c}'
     for p in listdir(path):
-        pasta  = f'{path.replace("exemples","tests/main_test")}/T_{p.replace(".c","")}'
-        
-        print(pasta)
+        caminho_do_arquivo = f'{path}/{p}'
+        nova_pasta = f'{path.replace("exemples","tests/main_test")}/T_{p.replace(".c","")}'
+        novo_caminho = f'{nova_pasta}/exec.c'
+        if isdir(nova_pasta):
+            print('ja existe',nova_pasta)
+            continue 
+        #fazendo alteracoes
