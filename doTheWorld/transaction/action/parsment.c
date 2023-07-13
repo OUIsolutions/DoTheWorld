58615d689f97c1e727b800593a9c27cf8802f9ed
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-13 19:04:23 -0300

    att

--- doTheWorld/transaction/action/parsment.c
+++ doTheWorld/transaction/action/parsment.c
@@ -8,13 +8,22 @@ char *DtwTransactionAction_convert_action(DtwTransactionAction *self) {
 
         case DTW_MODIFY_OR_CREATE_FILE:
             return "modify or create file";
+
         case DTW_MODIFY_FILE:
             return "modify file";
+
         case DTW_MODIFY_SYNC_FILE:
             return "modify sync file";
 
-        case DTW_RENAME_ANY:
-            return "rename any";
+        case DTW_CREATE_DIR:
+            return "create dir";
+
+        case DTW_CREATE_DIR_IF_DONT_EXIST:
+            return "create dir if dont exist";
+
+        case DTW_MOVE_ANY:
+            return "move any";
+
         case DTW_REMOVE_ANY:
             return "remove any";
 
@@ -43,9 +52,19 @@ int DtwTransactionAction_convert_string(const char *actionString) {
         return DTW_MODIFY_SYNC_FILE;
     }
 
-    else if (strcmp(actionString, "rename any") == 0) {
-        return DTW_RENAME_ANY;
+
+    else if (strcmp(actionString, "create dir") == 0) {
+        return DTW_CREATE_DIR;
     }
+
+    else if (strcmp(actionString, "create dir of dont exist") == 0) {
+        return DTW_CREATE_DIR_IF_DONT_EXIST;
+    }
+
+    else if (strcmp(actionString, "move any") == 0) {
+        return DTW_MOVE_ANY;
+    }
+
 
     else if (strcmp(actionString, "remove any") == 0){
         return DTW_REMOVE_ANY;
