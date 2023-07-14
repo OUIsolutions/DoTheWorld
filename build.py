191133d204b461e379137d4a9f393672be553d86
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-13 21:21:45 -0300

    at

--- build.py
+++ build.py
@@ -11,13 +11,13 @@ OUTPUT_TEST = 'tests/doTheWorld_test.h'
 OUTPUT = 'doTheWorld.h'
 
 
-'''
+
 ct.generate_amalgamated_code(STARTER,OUTPUT_TEST)
 test = ct.FolderTestPreset(folder='tests/main_test',side_effect_folder='tests/target')
 test.generate_ouptut()
 test.start_test()
 ct.include_code_in_markdown('README.md',save_file=True)
 ct.generate_amalgamated_code(STARTER,OUTPUT)
-'''
+
 create_exemples(TEST_NAME,OUTPUT)
 zip_folder()
