c85e90367049070014a41d054fa4dc2592644886
Mateus Moutinho Queiroz <61179749+mateusmoutinho@users.noreply.github.com>
2023-07-14 18:25:42 -0300

    att

--- teste.c
+++ teste.c
@@ -2,17 +2,8 @@
 #include "doTheWorld/doTheWorldMain.h"
 
 int main(int argc, char *argv[]){
+ 
+  dtw_create_dir_recursively("a/b/c");
 
-    //creating the b64 file
-    const char *blob_path = "tests/target/blob.png";
-    char *blob  = dtw_convert_binary_file_to_base64(blob_path);
-    long output;
-    unsigned char  *result = dtw_base64_decode(blob,&output);
-
-    dtw_write_any_content("tests/target/blob2.png",result,output);
-
-    free(result);
-    free(blob);
-
-    return 0;
+  return 0;
 }
