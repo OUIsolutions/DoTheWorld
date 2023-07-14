fd8c6554dd293e6d83f8170351bf87c835b38f6a
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-13 21:21:09 -0300

    at

--- Build/exemple_build.py
+++ Build/exemple_build.py
@@ -20,8 +20,8 @@ def move_all_c(destination:str,current_path:str,test_name:str,output:str):
             with open(path,'r') as arq:
                 content = arq.read()
 
-                content = content.replace(f'../../../{test_name}',test_name)
-                content = content.replace(f'../../{test_name}',test_name)
+                content = content.replace(f'../../../{test_name}',output)
+                content = content.replace(f'../../{test_name}',output)
 
             name =dirname(path).split('/')[-1].replace('T_','')
             with open(f'{destination}/{name}.c','w') as arq2:
