39d4cb104fc5d4db05a4dc7c23764f3840f03dc6
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-14 23:03:59 -0300

    fixing path problemns

--- teste.c
+++ teste.c
@@ -1,9 +1,11 @@
 
-#include "doTheWorld.h"
+#include "doTheWorld/doTheWorldMain.h"
 
 int main(int argc, char *argv[]){
  
-  dtw_create_dir_recursively("a/b/c");
-
+  DtwPath *path = newDtwPath("teste.txt");
+  path->add_start_dir(path,"test");
+  path->represent(path);
+  path->free(path);
   return 0;
 }
