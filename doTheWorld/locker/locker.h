243e33d75c369a631ba424347a74e6c347860caa
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-16 16:04:26 -0300

    att

---
+++ doTheWorld/locker/locker.h
@@ -0,0 +1,24 @@
+
+
+typedef struct DtwLocker{
+
+   char *path;
+   int process;
+   int max_lock_time;
+
+   bool (*lock)(struct DtwLocker *self,const  char *element);
+   void (*unlock)(struct DtwLocker *self,const  char *element);
+   void (*free)(struct DtwLocker *self);
+
+}DtwLocker;
+
+
+
+DtwLocker *newDtwLocker(char *path, int process,int max_lock_time);
+
+bool DtwLocker_lock(struct DtwLocker *self,const  char *element);
+
+void DtwLocker_unlock(struct DtwLocker *self,const  char *element);
+
+void DtwLocker_free(struct DtwLocker *self);
+
