a511e481db27fac6ef321cbcf26e7e3d21d1fbfc
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-24 10:54:03 -0300

    att

---
+++ tests/main_test/transaction/T_transaction_loading_from_json_wrong_json/exec.c
@@ -0,0 +1,17 @@
+//
+// Created by mateusmoutinho on 24/07/23.
+//
+#include "../../../doTheWorld_test.h"
+
+int main(){
+    DtwTransaction *t = newDtwTransaction_from_json_file("tests/target/wrong_transaction.json");
+    if(!t){
+        DtwJsonTransactionError *error = dtw_validate_json_transaction_file("tests/target/wrong_transaction.json");
+        error->represent(error);
+        error->free(error);
+        return 0;
+    }
+    
+    t->represent(t);
+    t->free(t);
+}
