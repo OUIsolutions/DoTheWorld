f4fc1988402cbd7ff4f61eb4bf80f83e784f3827
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-24 10:08:32 -0300

    at

---
+++ tests/main_test/transaction/T_transaction_not_executing/exec.c
@@ -0,0 +1,13 @@
+//
+// Created by mateusmoutinho on 24/07/23.
+//
+#include "../../../doTheWorld_test.h"
+
+int main(){
+    DtwTransaction *t = newDtwTransaction();
+    t->write_string(t,"b.txt","aaaaa");
+    t->move_any(t,"a.txt","c.txt");
+    t->delete_any(t,"blob.png");
+    t->copy_any(t,"sub_folder","sub_folder2");
+    t->commit(t,"tests/target");
+}
