2f34de10d900d41760f7c02a9f2b8e038c0fddda
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-25 17:28:32 -0300

    at

--- doTheWorld/database/database.c
+++ doTheWorld/database/database.c
@@ -17,7 +17,19 @@ DtwDataBase * newDtwDataBase(const char *path){
 }
 
 DtwJsonTransactionError *DtwDataBase_reconstruct(struct DtwDataBase *self);
-DtwJsonTransactionError *DtwDataBase_commit(struct DtwDataBase *self);
+DtwJsonTransactionError *DtwDataBase_commit(struct DtwDataBase *self){
+
+    self->locker->lock(self->locker,self->path);
+    long path_size = strlen(self->path);
+    //fix pending transactions
+    char *transaction_path = (char*) calloc(sizeof(char*), path_size + 50);
+    sprintf(transaction_path,"%s/pending_transaction.json",self->path);
+    
+    DtwTransaction *pending_transaction = newDtwTransaction_from_json_file(transaction_path);
+
+
+
+}
 void DtwDataBase_free(struct DtwDataBase *self){
     free(self->path);
     free(self->data_path);
