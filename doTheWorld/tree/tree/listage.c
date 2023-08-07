85e7a346760361a257fefefb7e3fc570df47694f
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-08-07 15:32:26 -0300

    at

--- doTheWorld/tree/tree/listage.c
+++ doTheWorld/tree/tree/listage.c
@@ -46,7 +46,52 @@ struct DtwStringArray *DtwTree_list_files(struct DtwTree *self, const char *path
     return formated_elements;
 }
 
-struct DtwStringArray *DtwTree_list_dirs(struct DtwTree *self, const char *path,bool concat_path);
+struct DtwStringArray *DtwTree_list_dirs(struct DtwTree *self, const char *path,bool concat_path){
+
+    DtwStringArray *formated_elements = newDtwStringArray();
+    for(int i = 0; i < self->size; i++){
+        DtwTreePart *current = self->tree_parts[i];
+        DtwPath *current_path = current->path;
+        char *current_path_string = current_path->get_path(current_path);
+
+        long size = strlen(current_path_string);
+        char last_char = current_path_string[size-1];
+        if(last_char !='/'){
+            free(current_path_string);
+            continue;
+        }
+
+        if(dtw_starts_with(current_path_string,path)){
+            //means its not an path from these dimension
+            int path_size = strlen(path);
+
+            int total_found = 0;
+            for(int i =path_size +1; i < size;i++){
+                if(current_path_string[i] == '/'){
+                    total_found+=1;
+                    continue;
+                }
+            }
+
+            if(total_found ==1){
+                formated_elements->append(formated_elements,current_path_string);
+            }
+
+        }
+
+        free(current_path_string);
+
+    }
+    if(!concat_path){
+
+        DtwStringArray  *not_concatened = private_dtw_remove_start_path(formated_elements,path);
+        formated_elements->free(formated_elements);
+        return not_concatened;
+    }
+
+
+    return formated_elements;
+}
 
 struct DtwStringArray *DtwTree_list_all(struct DtwTree *self, const char *path,bool concat_path);
 
