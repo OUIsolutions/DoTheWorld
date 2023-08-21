d14960ae0e9e225682224f1e11fdd3e9621ad21b
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-08-21 08:39:53 -0300

    att

---
+++ doTheWorld/resource/resource_array/resource_array.c
@@ -0,0 +1,28 @@
+
+
+
+DtwResourceArray * newDtwResourceArray(){
+    DtwResourceArray *self = (DtwResourceArray*) malloc(sizeof (DtwResourceArray));
+    self->resources = (DtwResource**) malloc(0);
+    self->size = 0;
+}
+
+
+void DtwResourceArray_append(DtwResourceArray *self, DtwResource *element){
+    self->resources = realloc(self->resources,(self->size +1) * sizeof (DtwResource**));
+    self->resources[self->size] = element;
+    self->size+=1;
+}
+
+void DtwResourceArray_pop(DtwResourceArray *self, long position){
+    DtwResource_free(self->resources[position]);
+    for(long i = position; i < self->size -1; i++){
+        self->resources[i] = self->resources[i+1];
+    }
+}
+
+void DtwResourceArray_free(DtwResourceArray *self, DtwResource *element){
+    for(int i = 0; i < self->size; i++){
+        DtwResource_free(self->resources[i]);
+    }
+}
--- doTheWorld/resource/resource_array/resource_array.h
+++ doTheWorld/resource/resource_array/resource_array.h
@@ -1,6 +1,18 @@
 
+
+
 typedef struct DtwResourceArray{
     DtwResource **resources;
     long size;
 
 }DtwResourceArray;
+
+DtwResourceArray * newDtwResourceArray();
+
+void DtwResourceArray_append(DtwResourceArray *self, DtwResource *element);
+
+void DtwResourceArray_pop(DtwResourceArray *self, long position);
+
+
+void DtwResourceArray_free(DtwResourceArray *self, DtwResource *element);
+
