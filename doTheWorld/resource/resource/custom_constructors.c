76504965d5e75abc1b85ba4aaf6514a7d60664c3
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-08-25 17:28:03 -0300

    att

--- doTheWorld/resource/resource/custom_constructors.c
+++ doTheWorld/resource/resource/custom_constructors.c
@@ -1,14 +1,19 @@
 
 
-DtwResource * DtwResource_append(DtwResource *self){
+DtwResource * DtwResource_append(DtwResource *self,char *end_path){
     long  size = dtw_get_total_itens_of_dir(self->path);
     if(size < 0){
         size = 0;
     }
     while(true){
 
-        char path[20] ={0};
-        sprintf(path,"%ld",size);
+        char path[300] ={0};
+        if(end_path){
+            sprintf(path,"%ld%s",size,end_path);
+        }
+        else{
+            sprintf(path,"%ld",size);
+        }
         DtwResource *new_element = DtwResource_sub_resource_ensuring_not_exist(self,path);
         if(new_element){
             return new_element;
@@ -18,10 +23,9 @@ DtwResource * DtwResource_append(DtwResource *self){
 
 
 }
-/*
+
 DtwResource * DtwResource_now(DtwResource *self);
 
 DtwResource * DtwResource_now_in_unix(DtwResource *self);
 
 DtwResource * DtwResource_random(DtwResource *self);
-*/
