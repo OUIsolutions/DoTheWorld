c8180e8f291be3032951f66eff5265b65059372c
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-08-28 00:55:42 -0300

    att

---
+++ tests/main_test/hash/T_file_hashing/exec.c
@@ -0,0 +1,20 @@
+#include "../../../doTheWorld_test.h"
+
+
+
+
+
+int main (){
+    DtwNamespace dtw = newDtwNamespace();
+
+    DtwHash *my_hash = dtw.hash.newHash();
+
+    dtw.hash.digest_file(my_hash,"tests/target/a.txt");
+
+    printf("after a file :%s\n",my_hash->hash);
+
+    dtw.hash.digest_folder_by_content(my_hash,"tests/target");
+    printf("after a folder %s\n",my_hash->hash);
+
+    dtw.hash.free(my_hash);
+}
