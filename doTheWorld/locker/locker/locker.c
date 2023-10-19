b4c7853ff3fae2288a4749b9dcd8ff53d5335f33
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-10-19 16:18:56 -0300

    at

--- doTheWorld/locker/locker/locker.c
+++ doTheWorld/locker/locker/locker.c
@@ -20,24 +20,23 @@ int DtwLocker_lock(struct DtwLocker *self, const  char *element,int max_time){
     DtwLockerStream  *stream = privatenewDtwLockerStream(self->shared_lock_file);
     int error = stream->error;
     if(error){
-        printf("pegou aqui\n");
 
         privatenewDtwLockerStream_free(stream);
         return error;
     }
-    const char *PROCESS = "p";
-    const char *FILE  = "f";
-    const char *LAST_UPDATE = "l";
+
+
     cJSON *elements = stream->elements;
-    cJSON *created_locked = cJSON_CreateObject();
-    cJSON_AddNumberToObject(created_locked,PROCESS,self->process);
-    cJSON_AddStringToObject(created_locked,FILE, element);
+
+    cJSON *created_locked = cJSON_CreateArray();
+    cJSON_AddItemToArray(created_locked, cJSON_CreateNumber(self->process));
+    cJSON_AddItemToArray(created_locked, cJSON_CreateString(element));
     long now = time(NULL);
-    cJSON_AddNumberToObject(created_locked,LAST_UPDATE, (double)now);
+    cJSON_AddItemToArray(created_locked,cJSON_CreateNumber(now));
     cJSON_AddItemToArray(elements,created_locked);
 
-
     privatenewDtwLockerStream_set_elements(stream,elements);
+
     privatenewDtwLockerStream_free(stream);
 
     return 0;
--- doTheWorld/locker/stream/stream.c
+++ doTheWorld/locker/stream/stream.c
@@ -2,8 +2,9 @@
 DtwLockerStream * privatenewDtwLockerStream(const char *file){
     DtwLockerStream *self = (DtwLockerStream*) malloc(sizeof (DtwLockerStream));
     *self = (DtwLockerStream){0};
+
     self->file = fopen(file,"r+");
-    if(!file){
+    if(!self->file){
         self->error = DTW_FILE_NOT_CORRECT;
         return self;
     }
@@ -38,9 +39,12 @@ DtwLockerStream * privatenewDtwLockerStream(const char *file){
         fclose(self->file);
         self->error =DTW_INTERNAL_ERROR;
     }
+
     char *content = (char*)malloc(size +1);
-    long bytes_read = fread(content,sizeof(char),size,self->file);
+
+    unsigned long bytes_read = fread(content,sizeof(char),size,self->file);
     if(bytes_read <=0 ){
+
         free(content);
         flock(self->fd, LOCK_UN);
         fclose(self->file);
@@ -48,7 +52,10 @@ DtwLockerStream * privatenewDtwLockerStream(const char *file){
         return self;
     }
 
+    content[size] = '\0';
+
     self->elements = cJSON_Parse(content);
+
     free(content);
 
     if(!self->elements){
@@ -56,18 +63,28 @@ DtwLockerStream * privatenewDtwLockerStream(const char *file){
         fclose(self->file);
         cJSON_Delete(self->elements);
         self->error = DTW_FILE_NOT_CORRECT;
+        return self;
     }
+
     if(self->elements->type != cJSON_Array){
         flock(self->fd, LOCK_UN);
         fclose(self->file);
         cJSON_Delete(self->elements);
         self->error = DTW_FILE_NOT_CORRECT;
     }
+
     return self;
 }
 
 void  privatenewDtwLockerStream_set_elements(DtwLockerStream *self,cJSON *elements){
     char *result  = cJSON_PrintUnformatted(elements);
+
+    if(fseek(self->file,0,SEEK_SET) == -1){
+        flock(self->fd, LOCK_UN);
+        fclose(self->file);
+        self->error =DTW_INTERNAL_ERROR;
+    }
+
     fwrite(result,sizeof(char), strlen(result),self->file);
     free(result);
 }
