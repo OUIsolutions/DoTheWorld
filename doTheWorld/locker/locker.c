93c07c23172040e20a0b375076c52cd06f3ff4fd
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-10-19 07:53:13 -0300

    at

--- doTheWorld/locker/locker.c
+++ doTheWorld/locker/locker.c
@@ -30,22 +30,40 @@ void DtwLocker_lock(struct DtwLocker *self, const char *element) {
     char  *formated_path = (char*)calloc(sizeof(char),strlen(element)+10);
     sprintf(formated_path,"%s.lock",element);
 
+
     char process_string[20] = {0};
     sprintf(process_string,"%d",self->process);
 
-    while (true){
-        FILE *file = fopen(formated_path,"r+");
-        if(file == NULL){
-            file = fopen(formated_path,"w");
-            int fd = fileno(file);
-            flock(fd, LOCK_EX);
-            if(lock == 0){
-                continue;
+    /*
+    while(true){
+        time_t  now = time(NULL);
+        long last_modification = dtw_get_entity_last_motification_in_unix(formated_path);
+        bool not_exist = (dtw_entity_type(formated_path) == DTW_NOT_FOUND);
+        bool exist = !not_exist;
+        bool expired = false;
+
+        if(exist){
+            expired = last_modification < (now - self->max_lock_time);
+        }
+
+        if(not_exist || expired){
+            FILE  *file = fopen()
+        }
+
+
+        if(exist){
+            long process_owner = dtw_load_long_file_content(formated_path);
+            if(process_owner == self->process ){
+                if(DtwStringArray_find_position(self->locked_elements,formated_path) == -1){
+                    DtwStringArray_append(self->locked_elements,formated_path);
+                    free(formated_path);
+                }
+                return;
             }
         }
-    }
-    free(formated_path);
 
+    }
+    */
 
 
 
