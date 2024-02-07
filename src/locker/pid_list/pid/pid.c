bfa0444849c4c8cbf35e5bbafb9d0a2e1d8b73ac
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-02-07 09:02:57 -0300

    att

---
+++ src/locker/pid_list/pid/pid.c
@@ -0,0 +1,11 @@
+
+DtwLockerPidElement * newDtwLockerPidElement(int pid){
+    DtwLockerPidElement *self  = (DtwLockerPidElement*) malloc(sizeof (DtwLockerPidElement));
+    self->pid = pid;
+    self->total_locked = 1;
+    return  self;
+}
+
+void DtwLockerPidElement_free(DtwLockerPidElement *self){
+    free(self);
+}
---
+++ src/locker/pid_list/pid_list/pid_list.c
@@ -0,0 +1,56 @@
+
+
+DtwLockerPidList * newDtwLockerPidList(){
+    DtwLockerPidList *self = (DtwLockerPidList*) malloc(sizeof (DtwLockerPidList));
+    self->size = 0;
+    self->pids = (DtwLockerPidElement**) malloc(0);
+}
+
+void DtwLockerPidList_add_pid(DtwLockerPidList *self,int pid){
+    for(int i =0; i  < self->size; i++){
+        DtwLockerPidElement *current = self->pids[i];
+        if(current->pid ==pid){
+            current->total_locked+=1;
+            return;
+        }
+    }
+
+    self->pids =(DtwLockerPidElement**) realloc(
+            self->pids,
+            (self->size+1) * sizeof (DtwLockerPidElement**)
+    );
+
+    DtwLockerPidElement *created = newDtwLockerPidElement(pid);
+    self->pids[self->size] = created;
+    self->size += 1;
+
+}
+
+int DtwLockerPidList_get_higher_pid(DtwLockerPidList *self){
+    DtwLockerPidElement *higher = NULL;
+
+    for(int i =0; i  < self->size; i++){
+
+        DtwLockerPidElement *current = self->pids[i];
+        if(i == 0){
+            higher = current;
+        }
+        if(current->total_locked > higher->total_locked){
+            higher = current;
+        }
+    }
+    if(higher){
+        return higher->pid;
+    }
+    return -1;
+}
+
+void DtwLockerPidList_free(DtwLockerPidList *self){
+    for(int i =0; i  < self->size; i++){
+
+        DtwLockerPidElement *current = self->pids[i];
+        DtwLockerPidElement_free(current);
+    }
+    free(self->pids);
+    free(self);
+}
