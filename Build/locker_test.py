338ae862b318bc8e4d9330168436388fbb741579
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-02-08 11:58:07 -0300

    att

--- Build/locker_test.py
+++ Build/locker_test.py
@@ -8,30 +8,32 @@ from os import remove
 def execute_locker_test():
     
     out = compile_project('tests/locker_test/locker_test.c')
-    r = ComandLineExecution(f'valgrind ./{out}')
-    
-    try:
-        with open('tests/target/append.txt','r') as arq:
-            content = arq.read()
-            lines = content.split('\n')
-            if len(lines) != 21:
-                raise Exception('locker overwritten information')
+    for i in range(0,10):
+        print(f"test  locker:{i} passed")
+        r = ComandLineExecution(f'valgrind ./{out}')
+        
+        try:
+            with open('tests/target/append.txt','r') as arq:
+                content = arq.read()
+                lines = content.split('\n')
+                if len(lines) != 21:
+                    raise Exception('locker overwritten information')
+                try:
+                    remove('tests/target/append.txt')
+                except:pass     
+        except Exception as e:
             try:
                 remove('tests/target/append.txt')
-            except:pass     
-    except Exception as e:
-        try:
-            remove('tests/target/append.txt')
-            pass 
-        except:pass 
-        raise e
-    
-    if 'Rerun with --leak-check' in r.output:
-        raise Exception('leaks on loker')
-    
-    replaced = r.output.replace('ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)','')
-    if 'ERROR SUMMARY' in replaced: 
-        raise Exception('Errors on locker')
+                pass 
+            except:pass 
+            raise e
+        
+        if 'Rerun with --leak-check' in r.output:
+            raise Exception('leaks on loker')
+        
+        replaced = r.output.replace('ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)','')
+        if 'ERROR SUMMARY' in replaced: 
+            raise Exception('Errors on locker')
 
-    print('passed: locker_tests.c')
-    
+        print('passed: locker_tests.c')
+        
