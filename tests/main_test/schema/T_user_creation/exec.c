09d82ce71519d563e12c9c2e7443aa39ecc63006
Mateus <mateusmoutinho01@gmail.com>
2024-05-12 13:17:23 -0300

    at

--- tests/main_test/schema/T_user_creation/exec.c
+++ tests/main_test/schema/T_user_creation/exec.c
@@ -1,24 +1,20 @@
 
 #include "../../../doTheWorld_test.h"
-DtwNamespace dtw;
 
-void  create_users(DtwSchema *schema,const char *name,const char *email,const char *password, int age){
-    DtwResource *user = dtw.schema.new_insertion(schema);
-    dtw.resource.set_string_in_sub_resource(user,"name",name);
-    dtw.resource.set_string_in_sub_resource(user,"email",email);
-    dtw.resource.set_string_sha_in_sub_resource(user,"password",password);
-    dtw.resource.set_long_in_sub_resource(user,"age",age);
-}
 
 int main(){
-    dtw = newDtwNamespace();
+    DtwNamespace  dtw = newDtwNamespace();
 
     DtwResource *database = dtw.resource.newResource("schema_database");
     DtwSchema  *users =dtw.resource.sub_schema(database,"users");
     dtw.schema.add_primary_key(users,"name");
     dtw.schema.add_primary_key(users,"email");
 
-    create_users(users,"mateus","mateusmoutinho01@gmail.com","1234",27);
+    DtwResource *user = dtw.schema.new_insertion(users);
+    dtw.resource.set_string_in_sub_resource(user,"name","mateus");
+    dtw.resource.set_string_in_sub_resource(user,"email","mateusmoutinho01@gmail.com");
+    dtw.resource.set_string_sha_in_sub_resource(user,"password","12345");
+    dtw.resource.set_long_in_sub_resource(user,"age",27);
 
     if(dtw.resource.error(database)){
         printf("error:%s",dtw.resource.get_error_message(database));
