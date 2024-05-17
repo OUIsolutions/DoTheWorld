e21dc78787efc641760cf8ae495cf0f8810318f5
Mateus <mateusmoutinho01@gmail.com>
2024-05-17 19:46:39 -0300

    att

--- src/string_functions/string_functions.c
+++ src/string_functions/string_functions.c
@@ -1,5 +1,98 @@
 
 
+void private_dtw_remove_double_bars_from_string_array(struct DtwStringArray*path){
+    for(int i =0;i< path->size;i++){
+        char *buffer = private_dtw_format_path(path->strings[i]);
+        free(path->strings[i]);
+        path->strings[i] = buffer;
+    }
+}
+
+char *dtw_concat_path(const char *path1, const char *path2){
+
+    if(!path1){
+        return strdup(path2);
+    }
+
+    if(!path2){
+        return strdup(path1);
+    }
+
+    char *path = (char *)malloc(strlen(path1) + strlen(path2) + 3);
+
+    if(dtw_ends_with(path1, "/") || dtw_ends_with(path1, "\\")){
+        sprintf(path,"%s%s",path1,path2);
+
+    }
+    else{
+        sprintf(path,"%s/%s",path1,path2);
+
+    }
+    return path;
+}
+
+char * private_dtw_format_vaarg(const char *expresion, va_list args){
+
+    va_list args_copy;
+    va_copy(args_copy, args);
+    long required_size = vsnprintf(NULL, 0,expresion,args_copy);
+    va_end(args_copy);
+    char *buffer = (char*)malloc(sizeof(char) * required_size + 2);
+    vsnprintf(buffer,sizeof (char) * required_size+1,expresion,args);
+    return buffer;
+}
+
+char *private_dtw_format_path(const char *path){
+    int path_size = (int)strlen(path);
+    char *buffer = malloc((path_size+2) * sizeof(char));
+    int buffer_size = 0;
+    bool already_concatened = false;
+    for(long i = 0; i < path_size;i++){
+        char current_char = path[i];
+
+        bool is_a_divisor =current_char == '\\' || current_char == '/';
+
+        if(is_a_divisor && already_concatened){
+            continue;
+        }
+        if(is_a_divisor){
+#ifdef __linux
+            buffer[buffer_size] = '/';
+#else
+            buffer[buffer_size] = '\\';
+#endif
+            buffer_size+=1;
+            already_concatened = true;
+            continue;
+        }
+
+        already_concatened = false;
+        buffer[buffer_size] = current_char;
+        buffer_size+=1;
+
+    }
+    buffer[buffer_size] = '\0';
+    return buffer;
+}
+
+
+char * private_dtw_sub_str(const char *str, long start,long end){
+    long size = end - start;
+    char *value = (char*)malloc(size+ 2);
+
+    for(long i = 0; i <size; i++){
+        value[i] = str[i+start];
+    }
+    value[size] = '\0';
+    return value;
+}
+
+int private_dtw_string_cmp(const void *a, const void *b){
+    const char *str_a = *(const char **)a;
+    const char *str_b = *(const char **)b;
+    return strcmp(str_a, str_b);
+}
+
 
 bool dtw_starts_with(const char *string, const char *prefix){
     if(!string || !prefix){
@@ -25,8 +118,8 @@ char *private_dtw_replace_string_once(const char *target, const char *old_elemen
 
     const char *pos = strstr(target, old_element);
 
-    int size_of_old_element = strlen(old_element);
-    int size_of_new_element = strlen(new_element);
+    int size_of_old_element = (int)strlen(old_element);
+    int size_of_new_element = (int)strlen(new_element);
     // Allocate memory for the new string
     char *result = (char *)malloc(strlen(target) + size_of_new_element - size_of_old_element + 1);
 
@@ -60,8 +153,8 @@ char* dtw_replace_string(const char *target, const char *old_element, const char
 
 
 char *private_dtw_change_beginning_of_string(const char *target,int start_element_to_remove_size, const char *new_element) {
-    int target_size = strlen(target);
-    int new_element_size = strlen(new_element);
+    int target_size = (int)strlen(target);
+    int new_element_size = (int)strlen(new_element);
     char *result = (char *)malloc(target_size- start_element_to_remove_size + new_element_size   +2);
     strcpy(result, new_element);
     char *new_target = (char *)malloc(target_size - start_element_to_remove_size + 2);
--- src/string_functions/string_functions.h
+++ src/string_functions/string_functions.h
@@ -1,4 +1,22 @@
 
+
+char *dtw_concat_path(const char *path1, const char *path2);
+
+DtwStringArray* private_dtw_remove_start_path(struct DtwStringArray *paths,const char *path_to_remove);
+
+char *private_dtw_format_path(const char *path);
+
+void private_dtw_remove_double_bars_from_string_array(struct DtwStringArray*path);
+
+char * private_dtw_format_vaarg(const char *expresion, va_list args);
+
+char *private_dtw_realoc_formatting(char *ptr,const char *format,...);
+
+char * private_dtw_sub_str(const char *str, long start,long end);
+
+int private_dtw_string_cmp(const void *a, const void *b);
+
+
 bool dtw_starts_with(const char *string, const char *prefix);
 bool dtw_ends_with(const char *string, const char *suffix);
 char *private_dtw_replace_string_once(const char *target, const char *old_element, const char *new_element);
