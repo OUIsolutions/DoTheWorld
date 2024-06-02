01dd86fd71b29fae76799a0c3efbe590fdfa9da1
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-06-02 16:48:30 -0300

    att

--- build/lua/amalgamation.lua
+++ build/lua/amalgamation.lua
@@ -111,61 +111,84 @@ end
 ---@return string
  function Generate_amalgamation(start_point)
 
+    local stack = {{}}
+    local current_stack = stack[1]
+    current_stack.start_point = start_point
+    current_stack.content = clib.load_string(start_point)
+    current_stack.size = clib.get_str_size(current_stack.content)
+    current_stack.inside_string = false
+    current_stack.waiting_include = false
+    current_stack.string_buffer = ""
+    current_stack.final_text = ""
 
-    local content = clib.load_string(start_point)
+    local stack_size = 1
 
-    local size = clib.get_str_size(content)
-    local inside_string = false
-    local waiting_include = false
-    local string_buffer = ""
-    local final_text = ""
-    for i=1,size do
-        local is_start_string = verify_if_is_start_string_char(content,i,inside_string)
-        if is_start_string  then
-        	inside_string = true
-        end
-
-        local is_end_string = verify_if_is_end_string_char(is_start_string,content,i,inside_string)
-
-       if include_char_to_string_buffer(is_start_string,is_end_string,inside_string) then
-            string_buffer = string_buffer..clib.get_char(content,i)
-        end
-
-       if include_string_buffer_to_final(waiting_include,is_end_string) then
-            final_text = final_text..'"'..string_buffer..'"'
-        end
-
-        if is_include_point(content,i,inside_string) then
-        	waiting_include = true
-        end
-
-        if anulate_inclusion(waiting_include,content,i) then
-            final_text = final_text.."#include "
-        	waiting_include = false
-        end
-
-        if include_char_to_final(waiting_include,inside_string) then
-        	final_text = final_text..clib.get_char(content,i)
-        end
+    while true do
 
 
+            for i=1,current_stack.size do
 
-        if make_recursive_call(waiting_include,is_end_string) then
-            local dir = clib.extract_dir(start_point)
-            local full_path = clib.concat_path(dir,string_buffer)
+                local is_start_string = verify_if_is_start_string_char(current_stack.content,i,current_stack.inside_string)
+                if is_start_string  then
+                    current_stack.inside_string = true
+                end
 
-            local acumulated = Generate_amalgamation(full_path)
-            final_text = final_text.. acumulated.."\n"
+                local is_end_string = verify_if_is_end_string_char(is_start_string,current_stack.content,i,current_stack.inside_string)
 
-        	waiting_include = false
-        end
+               if include_char_to_string_buffer(is_start_string,is_end_string,current_stack.inside_string) then
+                    current_stack.string_buffer = current_stack.string_buffer..clib.get_char(current_stack.content,i)
+                end
 
-        if is_end_string then
-           inside_string = false
-           string_buffer = ""
-        end
+               if include_string_buffer_to_final(current_stack.waiting_include,is_end_string) then
+                    current_stack.final_text = current_stack.final_text..'"'..current_stack.string_buffer..'"'
+                end
+
+                if is_include_point(current_stack.content,i,current_stack.inside_string) then
+                    current_stack.waiting_include = true
+                end
+
+                if anulate_inclusion(current_stack.waiting_include,current_stack.content,i) then
+                    current_stack.final_text = current_stack.final_text.."#include "
+                    current_stack.waiting_include = false
+                end
+
+                if include_char_to_final(current_stack.waiting_include,current_stack.inside_string) then
+                    current_stack.final_text = current_stack.final_text..clib.get_char(current_stack.content,i)
+                end
+
+
+                if make_recursive_call(current_stack.waiting_include,is_end_string) then
+                    local dir = clib.extract_dir(current_stack.start_point)
+                    local full_path = clib.concat_path(dir,current_stack.string_buffer)
+                    stack_size = stack_size +1
+                    current_stack = {}
+                    stack[stack_size] = current_stack
+                    current_stack.start_point = full_path
+                    current_stack.content = clib.load_string(full_path)
+                    current_stack.size = clib.get_str_size(current_stack.content)
+                    current_stack.inside_string = false
+                    current_stack.waiting_include = false
+                    current_stack.string_buffer = ""
+                    current_stack.final_text = ""
+                    clib.print("incremetou\n")
+                    break
+                end
+
+            end
+
+
+            if stack_size == 1 then
+            	return current_stack.final_text
+            end
+            local old_stack = stack[stack_size]
+            stack_size = stack_size -1
+            current_stack = stack[stack_size]
+
+            current_stack.final_text = current_stack.final_text.. old_stack.final_text.."\n"
+            current_stack.waiting_include = false
+            current_stack.inside_string = false
+            current_stack.string_buffer = ""
 
     end
 
-    return final_text
 end
