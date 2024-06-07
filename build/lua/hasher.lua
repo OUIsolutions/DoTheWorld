1a5045fa064e8bbb57f383bb914331c630a79888
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-06-07 09:56:56 -0300

    att

--- build/lua/hasher.lua
+++ build/lua/hasher.lua
@@ -7,12 +7,14 @@ function Generate_sha_from_folder_not_considering_empty_folders(folder)
 	for i=1,files_size do
 	    local current_file = files[i]
 	    local content = dtw.load_file(current_file)
+		hasher.digest(current_file)
 		hasher.digest(content)
 	end
 	local dirs,dirs_size = dtw.list_dirs_recursively(folder,false)
 	for i=1,dirs_size do
 	    local current_dir = dirs[i]
-		local ignore,size= dtw.list_files_recursively(current_dir,false)
+	    local dir_path = dtw.concat_path(folder,current_dir)
+		local ignore,size= dtw.list_files_recursively(dir_path,false)
 		if size > 0 then
 			hasher.digest(current_dir)
 		end
