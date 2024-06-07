3d8aac9a1291670a549d2f4fa2f20e63de01607a
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-06-07 06:04:43 -0300

    att

--- build/lua/locker_test.lua
+++ build/lua/locker_test.lua
@@ -1,8 +1,19 @@
 
+---@param content string
+local function  count_break_lines(content)
+	local size = clib.get_str_size(content)
+	local total =0
+	for i=1,size do
+		if clib.get_char(content,i) == "\n" then
+			total = total+1
+		end
+	end
+	return total
+end
 function  Execute_locker_test()
-	local locker_file = "tests/locker_test.c"
-	local locker_output = "tests/locker_test.out"
-	local compilation_comand = "gcc "..locker_file.."-o"..locker_output
+	local locker_file = "tests/locker_test/locker_test.c"
+	local locker_output = "tests/locker_test/locker_test.out"
+	local compilation_comand = "gcc "..locker_file.." -o "..locker_output
 	local status = clib.system_with_status(compilation_comand)
 
     if status ~= 0 then
@@ -11,10 +22,26 @@ function  Execute_locker_test()
     end
     local TOTAL_TESTS = 10
     for i=1,TOTAL_TESTS do
-       local content = clib.system_with_string("./"..locker_output)
-       clib.print(content)
+       clib.system_with_string("./"..locker_output)
+       local locker_write_path = dtw.concat_path(SIDE_EFFECT,"append.txt")
+       local locker_write_content = dtw.load_file(locker_write_path)
+       if locker_write_content == nil then
+       	    clib.print(ANSI_RED.."locker fail\n")
+       	    Rebase_side_effect()
+       	    clib.exit(1)
+       	    return
+       end
+       local total_break_lines = count_break_lines(locker_write_content)
 
+       if total_break_lines == 10 then
+       	  clib.print(ANSI_GREEN.."locker test "..i.." passed\n")
+       else
+            clib.print(ANSI_RED.."locker test fail\n")
+            Rebase_side_effect()
+            clib.exit(1)
+       end
     end
+
     Rebase_side_effect()
 
 
--- build/lua/main.lua
+++ build/lua/main.lua
@@ -4,25 +4,36 @@
 
 local function main()
 
-        if false then
-            local src_sha = dtw.generate_sha_from_folder_by_content(LIB_FOLDER)
-            local cache = NewCache(CACHE_POINT)
+        local src_sha = dtw.generate_sha_from_folder_by_content(LIB_FOLDER)
+        local cache = NewCache(CACHE_POINT)
 
-            local amalgamation_cache = cache.new_element("amalgamation",function ()
-                return Generate_amalgamation_recursive(START_POINT)
-            end).add_dependencie(src_sha)
-
-            local amalgamation_result = amalgamation_cache.perform()
-            dtw.write_file(END_TEST_POINT,amalgamation_result)
-
-            Execute_full_test(cache,src_sha)
-            Create_examples()
+        local amalgamation_cache = cache.new_element("amalgamation",function ()
+            return Generate_amalgamation_recursive(START_POINT)
+        end).add_dependencie(src_sha)
 
 
+        local amalgamation_result = amalgamation_cache.perform()
+        dtw.write_file(END_TEST_POINT,amalgamation_result)
 
-            clib.print(ANSI_RESET)
+        Execute_full_test(cache,src_sha)
 
-        end
+        local locker_content= dtw.load_file("tests/locker_test/locker_test.c")
+
+       cache.new_element("locker",function ()
+            Execute_locker_test()
+        end).
+        add_dependencie(locker_content).
+        add_dependencie(src_sha).
+        perform()
+
+
+        Create_examples()
+
+
+
+
+        clib.print(ANSI_RESET)
+
 
 
 
