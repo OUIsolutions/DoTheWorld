f67f6cef4cce73332fc65120e1e313c79063d2c7
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-06-24 15:50:28 -0300

    at

--- teste.c
+++ teste.c
@@ -1,43 +1,20 @@
 
 #include "src/one.c"
 DtwNamespace dtw;
-
-
-
-
 int main (){
-    DtwNamespace dtw = newDtwNamespace();
+    dtw = newDtwNamespace();
 
-    DtwResource *values = dtw.resource.newResource("tests/target/new_folder");
+    DtwResource *t = dtw.resource.newResource("data");
+    DtwResource *ar = dtw.resource.sub_resource(t,"a.txt");
 
-    DtwResource *string_element = dtw.resource.sub_resource(values,"text.txt");
-    dtw.resource.set_string(string_element,"nothing");
+    bool is_binary;
     long size;
-    unsigned char *blob = dtw_load_binary_content("tests/target/blob.png",&size);
-
-    DtwResource *blob_element = dtw.resource.sub_resource(values,"blob.png");
-    dtw.resource.set_binary(blob_element,blob,size);
-
-    free(blob);
-    DtwResource *bInt = dtw.resource.sub_resource(values,"b.txt");
-    dtw.resource.set_long(bInt,25);
-
-    DtwResource *cDouble = dtw.resource.sub_resource(values,"c.txt");
-    dtw.resource.set_double(cDouble,10.5);
-
-    DtwResource *dBool = dtw.resource.sub_resource(values,"b.txt");
-    dtw.resource.set_bool(dBool,true);
-
-    DtwResource  *sub_foder = dtw.resource.sub_resource(values,"sub_foder");
-    DtwResource *string_element2 = dtw.resource.sub_resource(sub_foder,"a.txt");
-    dtw.resource.set_string(string_element2,"nothing");
-
-
-    dtw.resource.commit(values);
-    dtw.resource.free(values);
-
-
-
-
-
+    unsigned char *a = dtw.load_any_content("a.txt",&size,&is_binary);
+    dtw.resource.set_any(ar,a,size,is_binary);
+    printf("a %s\n",dtw.resource.get_string(ar));
+    dtw.resource.free(ar);
+    dtw.resource.commit(t);
+    free(a);
 }
+
+
