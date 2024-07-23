9685de4090a8ee463c22ad473951203a670ef84e
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-07-23 01:13:24 -0300

    att

--- teste.c
+++ teste.c
@@ -1,34 +1,35 @@
 
 #define DTW_DEBUG_TIME
 #include "src/one.c"
+DtwNamespace dtw;
+DtwRandonizer *randonizer;
+void cria_x_usuarios(DtwResource *usuarios,int tamanho) {
 
+    for(int i = 0; i < tamanho; i++) {
+        DtwResource  *novo_usuario =  dtw.resource.sub_resource(usuarios,"%d",i);
+        char nome_usuario[50]  = {0};
+        sprintf(nome_usuario,"usuario%d",i);
+        char email[50] = {0};
+        sprintf(email,"%d@gmail.com",i);
+        dtw.resource.set_string_in_sub_resource(novo_usuario,"nome",nome_usuario);
+        dtw.resource.set_string_in_sub_resource(novo_usuario,"email",email);
+        int idade = dtw.randonizer.generate_num(randonizer,100);
+        dtw.resource.set_long_in_sub_resource(novo_usuario,"idade",idade);
 
+    }
+}
 int main(){
-    DtwNamespace dtw = newDtwNamespace();
+    dtw = newDtwNamespace();
+    randonizer = dtw.randonizer.newRandonizer();
 
-    DtwResource *teste = dtw.resource.newResource("teste");
-
-    DtwResource *sub_1 = dtw.resource.sub_resource(teste, "a");
-
-    dtw.resource.set_string(sub_1, "sla");
-
-    for(int i = 0; i < 20; i++){
-
-        DtwResource *sub_1 = dtw.resource.sub_resource(teste, "%d", i);
-
-        dtw.resource.set_string(sub_1, "sla");
-    }
+    dtw.remove_any("usuarios");
+    DtwResource *usuarios = dtw.resource.newResource("usuarios");
+    cria_x_usuarios(usuarios,100);
+    dtw.resource.commit(usuarios);
+    dtw.resource.free(usuarios);
+    dtw.randonizer.free(randonizer);
 
 
-    DtwResourceArray *list = dtw.resource.sub_resources(teste);
-
-    for(int i = 0; i < list->size; i++){
-        DtwResource *element = list->resources[i];
-
-        char *path = element->name;
-
-        printf("\n\tpath: %s\n", path);
-    }
     //dtw.resource.commit(teste);
 
 }
