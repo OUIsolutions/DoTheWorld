2a2a95d470cb611e844de42b24bc6df2c71f9a78
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-07-25 18:20:55 -0300

    att

--- src/functions/sha256/sha256.h
+++ src/functions/sha256/sha256.h
@@ -6,6 +6,8 @@ void calc_sha_256_from_string(uint8_t hash[SIZE_OF_SHA_256_HASH], const char *in
 
 int calc_sha_256_from_file(uint8_t hash[SIZE_OF_SHA_256_HASH], const char *filename);
 
+char * calc_sha_256_from_string_returning_string(const char *input);
+
 char * calc_sha_256_from_file_returning_string(const char *filename);
 
 char * sha256_open_file(const char *filename, int *size);
