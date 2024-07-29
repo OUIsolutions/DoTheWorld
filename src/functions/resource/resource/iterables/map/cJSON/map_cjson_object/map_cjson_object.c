ce4398bc293667a080cd0fbd2fcc6bc318324a88
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-07-29 05:46:45 -0300

    att

--- src/functions/resource/resource/iterables/map/cJSON/map_cjson_object/map_cjson_object.c
+++ src/functions/resource/resource/iterables/map/cJSON/map_cjson_object/map_cjson_object.c
@@ -1,21 +1,21 @@
 #include "../unique.definition_requirements.h"
 
 void  *private_dtw_cJSONObject_callback(DtwResource *item,void *args) {
-    private_DtwResource_cJSONObject_args *formmate_args = (private_DtwResource_cJSONObject_args*)args;
+    DtwResourcecJSONObjectMapProps *formmate_args = (DtwResourcecJSONObjectMapProps*)args;
     privateDtw_cJSON_element_and_key *created = (privateDtw_cJSON_element_and_key*)malloc(sizeof(privateDtw_cJSON_element_and_key));
-    created->key = formmate_args->key_provider(item,args);
+    created->key = formmate_args->key_provider_callback(item,args);
     created->element =formmate_args->callback(item,formmate_args->args);
     return (void*)created;
 }
 
 bool private_dtw_cJSONObject_filtrage(DtwResource *item,void *args) {
-    private_DtwResource_cJSONObject_args *formmate_args = (private_DtwResource_cJSONObject_args*)args;
-    return formmate_args->filtrage(item,formmate_args->args);
+    DtwResourcecJSONObjectMapProps *formmate_args = (DtwResourcecJSONObjectMapProps*)args;
+    return formmate_args->filtrage_callback(item,formmate_args->args);
 }
 
 int private_dtw_cJSONObject_ordenation(DtwResource *item1,DtwResource *item2,void *args) {
-    private_DtwResource_cJSONObject_args *formmate_args = (private_DtwResource_cJSONObject_args*)args;
-    return formmate_args->ordenation(item1,item2,formmate_args->args);
+    DtwResourcecJSONObjectMapProps *formmate_args = (DtwResourcecJSONObjectMapProps*)args;
+    return formmate_args->ordenation_callback(item1,item2,formmate_args->args);
 }
 
 void privateDtwResource_add_to_item_to_cJSONObject(void* object, void *item){
@@ -30,33 +30,21 @@ cJSON *DtwResource_map_cJSONObject(DtwResource *self,DtwResourcecJSONObjectMapPr
 
     cJSON *itens = cJSON_CreateObject();
 
-    private_DtwResource_cJSONObject_args formated_args = {0};
-    formated_args.callback = props.callback;
-    formated_args.filtrage = props.filtrage_callback;
-    formated_args.key_provider = props.key_provider_callback;
-    formated_args.ordenation = props.ordenation_callback;
-    formated_args.args = props.args;
-
-
-    bool (*filtrage)(DtwResource *,void *) = NULL;
-    if(props.filtrage_callback) {
-        filtrage = private_dtw_cJSONObject_filtrage;
-    }
-    int (*ordenation)(DtwResource *,DtwResource *,void *) = NULL;
-    if(ordenation_callback) {
-        ordenation = private_dtw_cJSONObject_ordenation;
-    }
-
-    DtwResource_map(self,
+    DtwResourceMapProps map_props = DtwResource_create_map_props(
         itens,
         privateDtwResource_add_to_item_to_cJSONObject,
-        filtrage,
-        ordenation,
-        private_dtw_cJSONObject_callback,
-        &formated_args,
-        start,
-        qtd
-        );
+        private_dtw_cJSONObject_callback
+    );
+    map_props.args = (void*)&props;
+    if(props.filtrage_callback) {
+        map_props.filtrage_callback = private_dtw_cJSONObject_filtrage;
+    }
+
+    if(props.ordenation_callback) {
+        map_props.ordenation_callback = private_dtw_cJSONObject_ordenation;
+    }
+
+    DtwResource_map(self,map_props);
 
     return itens;
 }
--- src/types/Resource/iterables/map/cJSON/all.h
+++ src/types/Resource/iterables/map/cJSON/all.h
@@ -1,4 +1,3 @@
-#include "cJSONArray_map.h"
 #include "cJSON_element_and_key.h"
 #include "cJSONArrayProps.h"
 #include "cJSONObjectProps.h"
