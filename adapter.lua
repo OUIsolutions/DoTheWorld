6d580a2c8311c4d91312cf684b2de1b61ace0588
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-09-29 15:50:15 -0300

    at

--- adapter.lua
+++ adapter.lua
@@ -1,5 +1,16 @@
+local dtw = require("luaDoTheWorld/luaDoTheWorld")
+
+
+function remove_all_includes(content)
+
+end
 
 function remove_all_includes_of_dir(dir)
-    
-
-end 
+    local elements = dtw.list_files_recursively(dir, true)
+    for i = 1, #elements do
+        local current = elements[i]
+        local content = dtw.loadfile(current)
+        local content_formmatted = remove_all_includes(content)
+        dtw.write_file(current, content_formmatted)
+    end
+end
