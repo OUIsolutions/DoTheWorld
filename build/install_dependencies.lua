10377eacf22d0c9699084478d1523de6df65bee6
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-15 22:31:28 -0300

    att

--- build/install_dependencies.lua
+++ build/install_dependencies.lua
@@ -1,40 +1,23 @@
-function install_dependencies()
-    local hasher = darwin.dtw.newHasher()
+
+function Install_dependencies()
+    local hasher = dtw.newHasher()
     hasher.digest_folder_by_content("dependencies")
-    local EXPECTED_SHA = '76163fc7939200269ecdb2d9cbf817fbf715503cad34355557a944cd0485d1ed'
-    if hasher.get_value() == EXPECTED_SHA then
+    local TARGET_HASH = "bf1f7e84f00fbcee177987c0f8247286ae45de2c04e64cbc1cc997fbeb6a12f6"
+    if hasher.get_value() == TARGET_HASH then
         return
     end
-
-    darwin.dtw.remove_any("dependencies")
-
-    os.execute("mkdir -p dependencies")
-    os.execute(
-        "curl -L https://github.com/OUIsolutions/BearSslSingle-Unit/releases/download/0.0.3/BearSSLSingleUnit.h -o dependencies/BearSSLSingleUnit.h ")
-    os.execute(
-        "curl -L https://github.com/OUIsolutions/BearSslSingle-Unit/releases/download/0.0.3/BearSSLSingleUnit.c -o dependencies/BearSSLSingleUnit.c")
-
-    os.execute(
-    "curl -L https://raw.githubusercontent.com/DaveGamble/cJSON/refs/tags/v1.7.18/cJSON.c -o dependencies/cJSON.c")
-    os.execute(
-    "curl -L https://raw.githubusercontent.com/DaveGamble/cJSON/refs/tags/v1.7.18/cJSON.h -o dependencies/cJSON.h")
-    ---- trusted anchors
-    darwin.dtw.remove_any("BearSSL")
-    os.execute("git clone https://www.bearssl.org/git/BearSSL")
-    os.execute("curl -L https://curl.se/ca/cacert.pem -o BearSSL/cacert.pem")
-    os.execute("cd BearSSL && make")
-    os.execute("chmod +x BearSSL/build/brssl")
-    os.execute("./BearSSL/build/brssl ta BearSSL/cacert.pem > dependencies/BearSSLTrustAnchors.h")
-
-    darwin.dtw.remove_any("BearSSL")
-    os.execute(
-        "curl -L https://github.com/SamuelHenriqueDeMoraisVitrio/UniversalSocket/releases/download/v0.2.7/UniversalSocket.c -o dependencies/UniversalSocket.c")
-    os.execute(
-        "curl -L https://github.com/SamuelHenriqueDeMoraisVitrio/UniversalSocket/releases/download/v0.2.7/UniversalSocket.h -o dependencies/UniversalSocket.h")
-
-
-
-    local new_hasher = darwin.dtw.newHasher()
+    local comands = {
+        "mkdir -p dependencies",
+        "curl -L https://raw.githubusercontent.com/DaveGamble/cJSON/refs/tags/v1.7.18/cJSON.c -o dependencies/cJSON.c",
+        "curl -L https://raw.githubusercontent.com/DaveGamble/cJSON/refs/tags/v1.7.18/cJSON.h -o dependencies/cJSON.h",
+        "curl -L https://raw.githubusercontent.com/amosnier/sha-2/565f65009bdd98267361b17d50cddd7c9beb3e6c/sha-256.c -o dependencies/sha-256.c  ",
+        "curl -L https://raw.githubusercontent.com/amosnier/sha-2/565f65009bdd98267361b17d50cddd7c9beb3e6c/sha-256.h -o dependencies/sha-256.h  ",
+        "curl -L https://github.com/OUIsolutions/CHashManipulator/releases/download/0.001/CHashManipulator.h -o dependencies/CHashManipulator.h "
+    }
+    for _, command in ipairs(comands) do
+        os.execute(command)
+    end
+    local new_hasher = dtw.newHasher()
     new_hasher.digest_folder_by_content("dependencies")
-    print("new hasher is: " .. new_hasher.get_value())
+    print("new hasher is: ", new_hasher.get_value())
 end
