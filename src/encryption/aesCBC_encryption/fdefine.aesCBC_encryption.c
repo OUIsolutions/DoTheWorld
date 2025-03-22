e40b51a7e1f6e13840f8144d0653867754579722
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-21 21:25:21 -0300

    finishing lambda implementation

--- src/encryption/aesCBC_encryption/fdefine.aesCBC_encryption.c
+++ src/encryption/aesCBC_encryption/fdefine.aesCBC_encryption.c
@@ -7,18 +7,32 @@
 
 
 
-void privteDtwAES_CBC_EncryptionInterface_encrypt_buffer(struct AES_ctx *ctx,uint8_t* buf, uint32_t length);
+void privteDtwAES_CBC_EncryptionInterface_encrypt_buffer(struct AES_ctx *ctx,uint8_t* buf, uint32_t length){
+    AES_ctx_set_iv(ctx,ctx->iv);
+}
 
-void privteDtwAES_CBC_EncryptionInterface_decrypt_buffer(struct AES_ctx *ctx,uint8_t* buf, uint32_t length);
+void privteDtwAES_CBC_EncryptionInterface_decrypt_buffer(struct AES_ctx *ctx,uint8_t* buf, uint32_t length){
+    AES_CBC_decrypt_buffer(ctx, buf, length);
+    AES_ctx_set_iv(ctx,ctx->iv);
+}
 
 DtwEncriptionInterface *newDtwAES_CBC_EncryptionInterface(const uint8_t* key,int key_size,const uint8_t *iv, int iv_size){
     
-    
+    if(iv_size == 0){
+        return NULL;
+    }
+
+    if(iv_size > 16){
+        return NULL;
+    }
+
+
     DtwEncriptionInterface *self = newDtwAES_RAW_EncryptionInterface(key,key_size);
     if(self == NULL){
         return NULL;
     }
-    
+    memcpy(self->iv,iv,iv_size);
+
     self->encrypt_buffer = privteDtwAES_CBC_EncryptionInterface_encrypt_buffer;
     self->decrypt_buffer = privteDtwAES_CBC_EncryptionInterface_decrypt_buffer;
     return self;
--- src/encryption/aesRAW_encryption/types.aesRAW_encryption.h
+++ src/encryption/aesRAW_encryption/types.aesRAW_encryption.h
@@ -10,7 +10,6 @@
 typedef struct privateDtwAES_RAW_EncryptionInterface{
      uint8_t key[16];
      struct AES_ctx  ctx;
-     uint8_t temp_iv[16];
      uint8_t iv[16];
      void (*encrypt_buffer)(struct AES_ctx *ctx,uint8_t* buf, uint32_t length);
      void (*decrypt_buffer)(struct AES_ctx *ctx,uint8_t* buf, uint32_t length);
