33031b71b6f244515800c1c2095f12d855996390
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-22 15:52:20 -0300

    att

--- teste.c
+++ teste.c
@@ -4,16 +4,16 @@
 void save_cryto_file(DtwEncriptionInterface *test_interface){
     const char *message = "mensagem de teste";
     printf("mensagem original %ld\n",strlen(message));
-    DtwEncriptionInterface_write_string_file_content(test_interface,"teste.txt",message);
+    DtwEncriptionInterface_write_string_file_content_b64(test_interface,"teste.txt",message);
 }
 char *load_cryto_file(DtwEncriptionInterface *test_interface){
-    return DtwEncriptionInterface_load_string_file_content(test_interface,"teste.txt");
+    return DtwEncriptionInterface_load_string_file_content_b64(test_interface,"teste.txt");
 }
 
 int main(){
  
-    const char *key  = "0123456789012345";
-    DtwEncriptionInterface *test_interface = newDtwAES_CBC_EncryptionInterface_str(key,NULL);
+    const char *key  = "teste";
+    DtwEncriptionInterface *test_interface = newDtwAES_ECB_EncryptionInterface_str(key);
 
     save_cryto_file(test_interface);
     char *decrypted = load_cryto_file(test_interface);
