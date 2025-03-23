03ac5d6855ab95cf3a82e5a4958b7c5c1fcda438
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-23 11:42:06 -0300

    improving v1 encryption

--- src/encryption/aesCustomCBC_v1_encryption/fdefine.aesCustomCB_v1_enrcryption .c
+++ src/encryption/aesCustomCBC_v1_encryption/fdefine.aesCustomCB_v1_enrcryption .c
@@ -5,11 +5,16 @@ DtwEncriptionInterface *newDtwAES_Custom_CBC_v1_interface(const char *key){
     if(key == NULL){
         return NULL;
     }
-    char *sha1 = dtw_generate_sha_from_string(key);
-    char *sha2 = dtw_generate_sha_from_string(sha1);
+    DtwHash *hash = newDtwHash();
+    DtwHash_digest_string(hash,"isiojerjwj92392-10i4i5i3954ii2323-2");
+    DtwHash_digest_string(hash,key);
+    char *sha1 = strdup(hash->hash);
+    DtwHash_digest_string(hash,"is439i023203bjgffgfokk,fiwjj2342124432");
+    char *sha2 = strdup(hash->hash);
     DtwEncriptionInterface *self = newDtwAES_CBC_EncryptionInterface((uint8_t*)sha1,16,(uint8_t*)sha2,16);
     free(sha1);
     free(sha2);
+    DtwHash_free(hash);
     return self;
 
 }
